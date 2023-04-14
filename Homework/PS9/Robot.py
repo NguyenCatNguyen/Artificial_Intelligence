1418f5152fb7a4a782295ab2dc9bf806a5841b74
NguyenCatNguyen <catngn11@gmail.com>
2023-04-14 18:37:01 -0500

    update

--- Homework/PS9/Robot.py
+++ Homework/PS9/Robot.py
@@ -87,6 +87,21 @@ def value_iteration(T, R, gamma, epsilon):
         if delta < epsilon:
             break
     return V
+# Find the optimal policy using the value_iteration function
+V = value_iteration(T, R, gamma, epsilon)
 
-K = value_iteration(T, lambda s,a,snext: 0, 0.9, 0.0001)
-print(K)
+
+def policy_iteration(mdp):
+    "Solve an MDP by policy iteration [Fig. 17.7]"
+    U = dict([(s, 0) for s in mdp.states])
+    pi = dict([(s, random.choice(mdp.actions(s))) for s in mdp.states])
+    while True:
+        U = policy_evaluation(pi, U, mdp)
+        unchanged = True
+        for s in mdp.states:
+            a = argmax(mdp.actions(s), lambda a: expected_utility(a,s,U,mdp))
+            if a != pi[s]:
+                pi[s] = a
+                unchanged = False
+        if unchanged:
+            return pi
